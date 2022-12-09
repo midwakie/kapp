d741f75b340f1ff968d7286f120e3c9469c6a009
Tibu Padmakumar <tibu@enginef.com>
2022-12-09 13:46:21 +0530

    test hardness

--- app/navigation/AuthStack.tsx
+++ app/navigation/AuthStack.tsx
@@ -21,6 +21,7 @@ import { retrieveSelectedLanguage } from 'app/utils/storageUtils';
 import AvatarModifier from 'app/screens/AvatarModifier';
 import AccountAction from 'app/screens/AccountAction';
 import LinkChild from 'app/screens/LinkChild';
+import TestHardness from 'app/screens/TestHardness';
 
 const Stack = createStackNavigator();
 const AuthenticationStack = createStackNavigator();
@@ -46,6 +47,16 @@ const AuthStack = () => {
 
   return (
     <AuthenticationStack.Navigator>
+      <Stack.Screen
+        name="TestHardness"
+        component={TestHardness}
+        options={{
+          // When logging out, a pop animation feels intuitive
+          // You can remove this if you want the default 'push' animation
+          animationTypeForReplace: isLoggedIn ? 'push' : 'pop',
+          headerShown: false,
+        }}
+      />
       <Stack.Screen
         name="Select Role"
         component={SelectRole}
