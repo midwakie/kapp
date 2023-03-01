0bf5f61fd2465e58eb7d1c86638bd6b37ca989b4
Rashid <rashid@wiizb.com>
2023-03-01 10:46:39 +0530

    twilio

--- app/services/twilio-api.ts
+++ app/services/twilio-api.ts
@@ -1,6 +1,14 @@
 /* eslint-disable prettier/prettier */
 import axios from 'axios';
+import ApiConfig from 'app/config/api-config';
+
 
 export const getToken = ( username ) => {
-  return axios.get(`http://192.168.1.14:3001/token/${username}`).then((twilioUser) => twilioUser.data.jwt);
+  return axios.get(`${ApiConfig.BASE_URL}${ApiConfig.GET_TOKEN}?identity=${username}`).then((response) => {
+    return response.data.data.jwt;
+  })
+  .catch((error) => {
+    console.error('Error getting token:', error);
+    return null;
+  });
 };
